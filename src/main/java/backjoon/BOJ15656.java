2fdf63c0bfadb066c559c7db68992de8e441cf47
nathan29849 <phs960418@gmail.com>
2022-12-10 17:53:11 +0900

    [20221210] 15656번 풀이 완료

---
+++ src/main/java/backjoon/BOJ15656.java
@@ -0,0 +1,56 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ15656 {
+
+	private static final List<List<Integer>> answer = new ArrayList<>();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] numbers = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(numbers);
+		List<Integer> arr = new ArrayList<>();
+		dfs(numbers, m, arr);
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < answer.size(); i++) {
+			for (int j = 0; j < m; j++) {
+				sb.append(answer.get(i).get(j))
+					.append(" ");
+			}
+
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+	private static void dfs(int[] numbers, int m, List<Integer> arr) {
+
+		if (arr.size() == m) {
+			answer.add(arr);
+			return;
+		}
+
+		for (int i = 0; i < numbers.length; i++) {
+			List<Integer> tmp = new ArrayList<>(arr);
+			tmp.add(numbers[i]);
+			dfs(numbers, m, tmp);
+		}
+	}
+
+}
